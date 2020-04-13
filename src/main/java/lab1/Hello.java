22a3bedb57eb07389c20237f0bc8c53e04c33df1
Asher Didier <didiera@wwu.edu>
2020-04-12 21:56:47 -0700

    Hello now prints command line arg

--- src/main/java/lab1/Hello.java
+++ src/main/java/lab1/Hello.java
@@ -2,6 +2,10 @@ package lab1;
 
 public class Hello {
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+	System.out.print("Hello, ");
+	for (int index = 0; index < args.length-1; index++) {
+		System.out.print(args[index] + " ");
+	}
+	System.out.print(args[args.length-1] + "!");
   }
 }
